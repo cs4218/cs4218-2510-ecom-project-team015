1a808c7078445c2908b3084b16f18aad294dca19
erohsikivar <ravikishore1809@gmail.com>
2025-10-04 00:48:21 +0800

    Fix bugs in the contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,31 +1,32 @@
 import React from "react";
 import Layout from "./../components/Layout";
 import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
+
 const Contact = () => {
   return (
-    <Layout title={"Contact us"}>
+    <Layout title={"Contact Us - Ecommerce app"}>
       <div className="row contactus ">
+        <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
         <div className="col-md-6 ">
           <img
             src="/images/contactus.jpeg"
-            alt="contactus"
+            alt="Contact Us Image"
             style={{ width: "100%" }}
           />
         </div>
         <div className="col-md-4">
-          <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
           <p className="text-justify mt-2">
             For any query or info about product, feel free to call anytime. We are
             available 24X7.  
           </p>
           <p className="mt-3">
-            <BiMailSend /> : www.help@ecommerceapp.com
+            <BiMailSend data-testid="email-icon"/> : www.help@ecommerceapp.com
           </p>
           <p className="mt-3">
-            <BiPhoneCall /> : 012-3456789
+            <BiPhoneCall data-testid="phone-icon"/> : 012-3456789
           </p>
           <p className="mt-3">
-            <BiSupport /> : 1800-0000-0000 (toll free)
+            <BiSupport data-testid="support-icon"/> : 1800-0000-0000 (Toll Free)
           </p>
         </div>
       </div>
