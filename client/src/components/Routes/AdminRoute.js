489bf71a5961183e862888191a3a18f1979f874d
adhigop13 <adhigop13@gmail.com>
2025-10-03 23:53:58 +0800

    Fix bug in AdminRoute.js causing uncaught runtime exception screen to appear when trying to access admin routes from non admin user

--- client/src/components/Routes/AdminRoute.js
+++ client/src/components/Routes/AdminRoute.js
@@ -11,10 +11,15 @@ export default function AdminRoute(){
 
     useEffect(()=> {
         const authCheck = async() => {
-            const res = await axios.get("/api/v1/auth/admin-auth");
-            if(res.data.ok){
-                setOk(true);
-            } else {
+            try {
+                const res = await axios.get("/api/v1/auth/admin-auth");
+                if(res.data?.ok){
+                    setOk(true);
+                } else {
+                    setOk(false);
+                }
+            } catch (error) {
+                console.error("Authentication check from backend failed: ", error.message);
                 setOk(false);
             }
         };
