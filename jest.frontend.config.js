54824bacec21979dd1582835588b8961d9a6dbd7
erohsikivar <ravikishore1809@gmail.com>
2025-10-05 14:00:59 +0800

    Update frontend test config for forgot password

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -36,7 +36,6 @@ export default {
 		"!client/src/context/cart.js", // Excluded as it is not yet implemented
 		"client/src/pages/admin/*.js",
 		"client/src/pages/Auth/*.js",
-		"!client/src/pages/Auth/ForgotPassword.js", // Excluded as it is not yet implemented	
 		"client/src/pages/user/*.js",
 		"client/src/pages/*.js",
 		"!client/src/pages/CartPage.js", // Excluded as it is not yet implemented
