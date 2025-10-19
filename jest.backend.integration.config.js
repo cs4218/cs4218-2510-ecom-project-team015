9e9c75f53e4d6a166dfef1d1ae8ad9eda17a7a93
adhigop13 <adhigop13@gmail.com>
2025-10-19 12:48:36 +0800

    add configs for integration tests

---
+++ jest.backend.integration.config.js
@@ -0,0 +1,17 @@
+// jest.backend.integration.config.js
+export default {
+  displayName: "beintegration",
+
+  // use 'node' for backend-like integration tests, 'jsdom' for frontend React integration
+  testEnvironment: "node",
+
+  // setup file for initializing things like MSW or global mocks
+  setupFilesAfterEnv: ["<rootDir>/tests/setupIntegration.js"],
+
+  // all integration tests
+  testMatch: ["<rootDir>/tests/integration/*.test.js"],
+
+  // optionally, add coverage thresholds if you want
+  collectCoverage: true,
+  collectCoverageFrom: ["tests/integration/**"],
+};
